49f0ef55b2921c6a02c2a5e2104ae132f1c7a636
Juan5 <juangm2302@gmail.com>
2023-10-06 19:02:09 -0600

    Controllers&ModelsUpdated

--- Server/src/controllers/login.js
+++ Server/src/controllers/login.js
@@ -1,12 +1,33 @@
-const users = require("../utils/users");
+const { User } = require("../DB_connection");
 
-module.exports = (req, res) => {
-  const { email, password } = req.query;
-  let access = false;
-  users.forEach((user) => {
-    if (user.email === email && user.password === password) {
-      access = true;
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (email && password) {
+      const actualUser = await User.findOne({ where: { email: email } });
+      //* actualUser // { id, email, password } || null
+      if (actualUser) {
+        if (actualUser.password === password) {
+          return res.status(200).json({ access: true });
+        }
+        return res.status(400).send("Contraseña incorrecta");
+      }
+      return res.status(400).send("Usuario no encontrado");
     }
-  });
-  return res.status(200).json({ access });
+    return res.status(400).send("Faltan datos");
+  } catch (err) {
+    return res.status(500).send(err.message);
+  }
 };
+
+module.exports = login;
+
+//   let access = false;
+//   users.forEach((user) => {
+//     if (user.email === email && user.password === password) {
+//       access = true;
+//     }
+//   });
+//   return res.status(200).json({ access });
+// };
